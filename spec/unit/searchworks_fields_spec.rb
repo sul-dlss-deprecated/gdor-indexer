8dc5034edd9f957c0a06cce31730a75f515806dd
Naomi Dushay <ndushay@stanford.edu>
2012-12-20 16:15:54 -0800

    add author fields to searchworks_fields mixin; utilize sw_title methods from stanford-mods

--- spec/unit/searchworks_fields_spec.rb
+++ spec/unit/searchworks_fields_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 # FIXME:  should all these be required, chain-wise, in Indexer class?
 require 'searchworks_fields'
+require 'stanford-mods/searchworks'
 
 describe 'SearchworksFields mixin for SolrDocBuilder class' do
 
@@ -23,36 +24,48 @@ describe 'SearchworksFields mixin for SolrDocBuilder class' do
                       <titleInfo type='alternative'>
                         <title>and again</title>
                       </titleInfo>
-                      </mods>"
+                    </mods>"
       @smr.from_str(title_mods)
       @sdb = SolrDocBuilder.new(@fake_druid, @smr)
     end
     context "search fields" do
       it "title_245a_search" do
         @sdb.title_245a_search.should == "The Jerk"
+        @smr.should_receive(:sw_short_title) # from stanford-mods gem
+        @sdb.title_245a_search
       end
       it "title_245_search" do
         @sdb.title_245_search.should == "The Jerk A Tale of Tourettes"
+        @smr.should_receive(:sw_full_title) # from stanford-mods gem
+        @sdb.title_245_search
       end
       it "title_variant_search" do
         @sdb.title_variant_search.should == ["ta da!", 'and again']
+        @smr.should_receive(:sw_addl_titles) # from stanford-mods gem
+        @sdb.title_variant_search
       end
-      it "title_related_search should not be populated from mods" do
+      it "title_related_search should not be populated from MODS" do
         expect { @sdb.title_related_search }.to raise_error(NoMethodError)
       end
     end
     context "display fields" do
       it "title_display" do
         @sdb.title_display.should == "The Jerk A Tale of Tourettes"
+        @smr.should_receive(:sw_full_title) # from stanford-mods gem
+        @sdb.title_display
       end
       it "title_245a_display" do
         @sdb.title_245a_display.should == "The Jerk"
+        @smr.should_receive(:sw_short_title) # from stanford-mods gem
+        @sdb.title_245a_display
       end
-      it "title_245c_display should not be populated from mods" do
+      it "title_245c_display should not be populated from MODS" do
         expect { @sdb.title_245c_display }.to raise_error(NoMethodError)
       end
       it "title_full_display" do
         @sdb.title_full_display.should == "The Jerk A Tale of Tourettes"
+        @smr.should_receive(:sw_full_title) # from stanford-mods gem
+        @sdb.title_full_display
       end
       it "title_variant_display should not be populated - it is a copy field" do
         expect { @sdb.title_variant_display }.to raise_error(NoMethodError)
@@ -60,11 +73,81 @@ describe 'SearchworksFields mixin for SolrDocBuilder class' do
     end
     it "title_sort" do
       @sdb.title_sort.should == "Jerk A Tale of Tourettes"
+      @smr.should_receive(:sw_sort_title) # from stanford-mods gem
+      @sdb.title_sort
     end
   end
   
   context "author fields" do
-    
+    before(:all) do
+      name_mods = "<mods xmlns='#{Mods::MODS_NS}'>
+                      <name type='personal'>
+                        <namePart type='given'>John</namePart>
+                        <namePart type='family'>Huston</namePart>
+                        <role><roleTerm type='code' authority='marcrelator'>drt</roleTerm></role>
+                        <displayForm>q</displayForm>
+                      </name>
+                      <name type='personal'><namePart>Crusty The Clown</namePart></name>
+                      <name type='corporate'><namePart>Watchful Eye</namePart></name>
+                      <name type='corporate'>
+                        <namePart>Exciting Prints</namePart>
+                        <role><roleTerm type='text'>lithographer</roleTerm></role>
+                      </name>
+                      <name type='conference'><namePart>conference</namePart></name>
+                    </mods>"
+      @smr.from_str(name_mods)
+      @sdb = SolrDocBuilder.new(@fake_druid, @smr)
+    end
+    context "search fields" do
+      it "author_1xx_search" do
+        @sdb.author_1xx_search.should == "Crusty The Clown"
+        @smr.should_receive(:sw_main_author) # from stanford-mods gem
+        @sdb.author_1xx_search
+      end
+      it "author_7xx_search" do
+        @sdb.author_7xx_search.should == ["q", "Watchful Eye", "Exciting Prints", "conference"]
+        @smr.should_receive(:sw_addl_authors) # from stanford-mods gem
+        @sdb.author_7xx_search
+      end
+      it "author_8xx_search should not be populated from MODS" do
+        expect { @sdb.author_8xx_search }.to raise_error(NoMethodError)
+      end
+    end
+    context "facet fields" do
+      it "author_person_facet" do
+        @sdb.author_person_facet.should == ["q", "Crusty The Clown"]
+        @smr.should_receive(:sw_person_authors) # from stanford-mods gem
+        @sdb.author_person_facet
+      end
+      it "author_other_facet" do
+        @sdb.author_other_facet.should == ["Watchful Eye", "Exciting Prints", "conference"]
+        @smr.should_receive(:sw_impersonal_authors) # from stanford-mods gem
+        @sdb.author_other_facet
+      end
+    end
+    context "display fields" do
+      it "author_person_display" do
+        @sdb.author_person_display.should == ["q", "Crusty The Clown"]
+      end
+      it "author_person_full_display" do
+        @sdb.author_person_full_display.should == ["q", "Crusty The Clown"]
+      end
+      it "author_corp_display" do
+        @sdb.author_corp_display.should == ["Watchful Eye", "Exciting Prints"]
+        @smr.should_receive(:sw_corporate_authors) # from stanford-mods gem
+        @sdb.author_corp_display
+      end
+      it "author_meeting_display" do
+        @sdb.author_meeting_display.should == ["conference"]
+        @smr.should_receive(:sw_meeting_authors) # from stanford-mods gem
+        @sdb.author_meeting_display
+      end
+    end
+    it "author_sort" do
+      @sdb.author_sort.should == "Crusty The Clown"
+      @smr.should_receive(:sw_sort_author) # from stanford-mods gem
+      @sdb.author_sort
+    end
   end
   
   context "pub date fields" do
