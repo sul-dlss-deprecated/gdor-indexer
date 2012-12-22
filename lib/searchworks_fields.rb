ab586b5dbd8ad55949698e19cb1be8529acd67e8
Naomi Dushay <ndushay@stanford.edu>
2012-12-21 20:29:25 -0800

    add format implementation to searchworks_fields

--- lib/searchworks_fields.rb
+++ lib/searchworks_fields.rb
@@ -4,4 +4,32 @@ require 'stanford-mods'
 # Methods for SearchWorks Solr field values
 class SolrDocBuilder
 
+  # select one or more format values from the controlled vocabulary here:
+  #   http://searchworks-solr-lb.stanford.edu:8983/solr/select?facet.field=format&rows=0&facet.sort=index
+  # based on the dor_content_type  and/or what's in the Mods
+  # @return [String] value in the SearchWorks controlled vocabulary
+  def format
+    if dor_content_type == 'image'
+      'Image'
+    end
+  end
+
+  protected #---------------------------------------------------------------------
+  
+  # the value of the type attribute for a DOR object's contentMetadata
+  #  more info about these values is here:
+  #    https://consul.stanford.edu/display/chimera/DOR+content+types%2C+resource+types+and+interpretive+metadata
+  #    https://consul.stanford.edu/display/chimera/Summary+of+Content+Types%2C+Resource+Types+and+their+behaviors
+  # @return [String] 
+  def dor_content_type
+    content_md.xpath('@type').text
+  end
+  
+  # the contentMetadata for this object, derived from the public_xml
+  # @return [Nokogiri::XML::Element] containing the contentMetadata
+  def content_md 
+    @content_md ||= public_xml.root.xpath('/publicObject/contentMetadata').first
+  end
+  
+  
 end
--- spec/unit/searchworks_fields_spec.rb
+++ spec/unit/searchworks_fields_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 # FIXME:  should all these be required, chain-wise, in Indexer class?
 require 'searchworks_fields'
-require 'stanford-mods/searchworks'
+#require 'stanford-mods/searchworks'
 
 describe 'SearchworksFields mixin for SolrDocBuilder class' do
 
@@ -9,6 +9,29 @@ describe 'SearchworksFields mixin for SolrDocBuilder class' do
     @fake_druid = 'oo000oo0000'
     @smr = Stanford::Mods::Record.new
   end
+
+  context "fields from and methods pertaining to contentMetadata" do
+    before(:all) do
+      @type = 'image'
+      @cmd_xml = "<contentMetadata type='#{@type}' objectId='#{@fake_druid}'></contentMetadata>"
+      pub_xml = "<publicObject id='druid#{@fake_druid}'>#{@cmd_xml}</publicObject>"
+      @sdb = SolrDocBuilder.new(@fake_druid, @smr, Nokogiri::XML(pub_xml))
+    end
+    it "content_md should get the contentMetadata from the public_xml" do
+      content_md = @sdb.send(:content_md)
+      content_md.should be_an_instance_of(Nokogiri::XML::Element)
+      content_md.name.should == 'contentMetadata'
+# NOTE:  the below isn't working -- probably due to Nokogiri attribute bug introduced      
+  #    content_md.should be_equivalent_to(@cmd_xml)
+    end
+    it "dor_content_type should be the value of the type attribute on the contentMetadata element" do
+      @sdb.send(:dor_content_type).should == @type
+    end
+    it "format should be Image for contentMetadata type of image" do
+      @sdb.format.should == 'Image'
+    end
+  end
+  
 
   context "pub date fields" do
     before(:all) do
