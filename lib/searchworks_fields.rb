0c91b967c49ca7663591fc836ef775aa9e658798
Naomi Dushay <ndushay@stanford.edu>
2012-12-23 00:05:10 -0800

    implement display_type in searchworks_fields and add error logging to format

--- lib/searchworks_fields.rb
+++ lib/searchworks_fields.rb
@@ -1,5 +1,3 @@
-require 'stanford-mods'
- 
 # A mixin to the SolrDocBuilder class.
 # Methods for SearchWorks Solr field values
 class SolrDocBuilder
@@ -9,8 +7,23 @@ class SolrDocBuilder
   # based on the dor_content_type  and/or what's in the Mods
   # @return [String] value in the SearchWorks controlled vocabulary
   def format
+    # information on DOR content types:
+    #   https://consul.stanford.edu/display/chimera/DOR+content+types%2C+resource+types+and+interpretive+metadata
     if dor_content_type == 'image'
       'Image'
+    else
+      logger.error "Object #{druid} has unrecognized DOR content type: #{dor_content_type}"
+    end
+  end
+  
+  #  TODO:  ask Jessie if this Solr field is still used by SearchWorks
+  def display_type
+    if collection?
+      'collection'
+    elsif dor_content_type
+      dor_content_type
+    else
+      logger.error "Object #{druid} has no DOR content type (possibly missing type attribute on <contentMetadata> element)"
     end
   end
 
@@ -22,7 +35,7 @@ class SolrDocBuilder
   #    https://consul.stanford.edu/display/chimera/Summary+of+Content+Types%2C+Resource+Types+and+their+behaviors
   # @return [String] 
   def dor_content_type
-    content_md.xpath('@type').text
+    @dor_content_type ||= content_md.xpath('@type').text
   end
   
   # the contentMetadata for this object, derived from the public_xml
--- spec/unit/searchworks_fields_spec.rb
+++ spec/unit/searchworks_fields_spec.rb
@@ -1,22 +1,20 @@
 require 'spec_helper'
 # FIXME:  should all these be required, chain-wise, in Indexer class?
 require 'searchworks_fields'
-#require 'stanford-mods/searchworks'
 
 describe 'SearchworksFields mixin for SolrDocBuilder class' do
 
   before(:all) do
     @fake_druid = 'oo000oo0000'
     @smr = Stanford::Mods::Record.new
+    @ns_decl = "xmlns='#{Mods::MODS_NS}'"
+    @cmd_type = 'image'
+    @cmd_xml = "<contentMetadata type='#{@cmd_type}' objectId='#{@fake_druid}'></contentMetadata>"
+    @pub_xml = "<publicObject id='druid#{@fake_druid}'>#{@cmd_xml}</publicObject>"
+    @sdb = SolrDocBuilder.new(@fake_druid, @smr, Nokogiri::XML(@pub_xml), nil)
   end
 
   context "fields from and methods pertaining to contentMetadata" do
-    before(:all) do
-      @type = 'image'
-      @cmd_xml = "<contentMetadata type='#{@type}' objectId='#{@fake_druid}'></contentMetadata>"
-      pub_xml = "<publicObject id='druid#{@fake_druid}'>#{@cmd_xml}</publicObject>"
-      @sdb = SolrDocBuilder.new(@fake_druid, @smr, Nokogiri::XML(pub_xml))
-    end
     it "content_md should get the contentMetadata from the public_xml" do
       content_md = @sdb.send(:content_md)
       content_md.should be_an_instance_of(Nokogiri::XML::Element)
@@ -25,13 +23,43 @@ describe 'SearchworksFields mixin for SolrDocBuilder class' do
   #    content_md.should be_equivalent_to(@cmd_xml)
     end
     it "dor_content_type should be the value of the type attribute on the contentMetadata element" do
-      @sdb.send(:dor_content_type).should == @type
+      @sdb.send(:dor_content_type).should == @cmd_type
     end
-    it "format should be Image for contentMetadata type of image" do
-      @sdb.format.should == 'Image'
+    
+    context "format" do
+      it "format should be Image for contentMetadata type of image" do
+        @sdb.format.should == 'Image'
+      end
+      it "should log an error message for an unrecognized contentMetadata type" do
+        @sdb.stub(:dor_content_type).and_return('bogus')
+        @sdb.logger.should_receive(:error).with(/unrecognized DOR content type.*bogus/)
+        @sdb.format
+      end
+    end
+    
+    context "display_type" do
+      it "should be 'collection' if solr_doc_builder.collection?" do
+        coll_mods_xml = "<mods #{@ns_decl}><typeOfResource collection='yes'/></mods>"
+        @smr.from_str coll_mods_xml
+        sdb = SolrDocBuilder.new(@fake_druid, @smr, Nokogiri::XML(@pub_xml), nil)
+        sdb.display_type.should == 'collection'
+      end
+      it "should be the same as <contentMetadata> type attribute if it's not a collection" do
+        # NOTE: from the contentMetadata, not the mods
+        m = "<mods #{@ns_decl}><typeOfResource>sound recording</typeOfResource></mods>"
+        @smr.from_str m
+        sdb = SolrDocBuilder.new(@fake_druid, @smr, Nokogiri::XML(@pub_xml), nil)
+        @sdb.display_type.should == @cmd_type
+        @sdb.stub(:dor_content_type).and_return('bogus')
+        @sdb.display_type.should == 'bogus'
+      end
+      it "should log an error message if contentMetadata has no type" do
+        @sdb.stub(:dor_content_type).and_return(nil)
+        @sdb.logger.should_receive(:error).with(/has no DOR content type/)
+        @sdb.display_type
+      end
     end
   end
-  
 
   context "pub date fields" do
     before(:all) do
@@ -49,7 +77,7 @@ describe 'SearchworksFields mixin for SolrDocBuilder class' do
                       </titleInfo>
                     </mods>"
       @smr.from_str(title_mods)
-      @sdb = SolrDocBuilder.new(@fake_druid, @smr, nil)
+      @sdb = SolrDocBuilder.new(@fake_druid, @smr, nil, nil)
     end
   
     it "pub_date" do
