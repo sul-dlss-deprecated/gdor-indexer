6615851a3f0557bedeffd255093728cc0cd1678d
Naomi Dushay <ndushay@stanford.edu>
2013-01-04 00:05:02 -0800

    implemented geographic_search

--- lib/mods_fields.rb
+++ lib/mods_fields.rb
@@ -15,15 +15,40 @@ class SolrDocBuilder
     vals
   end
 
-  # NAOMI_MUST_COMMENT_THIS_METHOD
+  # Values are the contents of:
+  #   subject/geographic
+  #   subject/hierarchicalGeographic
+  #   subject/geographicCode  (only include the translated value if it isn't already present from other mods geo fields)
+  # @return [Array<String>] values for the geographic_search Solr field for this document or nil if none
   def geographic_search
-    
+    vals = @smods_rec.term_values([:subject, :geographic]) || []
+    gvals = @smods_rec.term_values([:subject, :hierarchicalGeographic])
+    vals.concat(gvals) if gvals
+    cvals = @smods_rec.subject.geographicCode.translated_value
+    if cvals
+      cvals.each { |val|  
+        vals << val if !vals.include?(val)
+      }
+    end
+
+    # print a message for any unrecognized encodings
+    codes = @smods_rec.term_values([:subject, :geographicCode]) 
+    if codes && codes.size > cvals.size
+      @smods_rec.subject.geographicCode.each { |n|
+        if n.authority != 'marcgac' && n.authority != 'marccountry'
+          @logger.info("#{@druid} has subject geographicCode element with untranslated encoding (#{n.authority}): #{n.to_xml}")
+        end
+      }
+    end
+
+    return nil if vals.empty?
+    vals    
   end
 
   # Values are the contents of:
   #   subject/temporal
   #   subject/genre
-  # @return [Array<String>] values for the topic_search Solr field for this document or nil if none
+  # @return [Array<String>] values for the subject_other_subvy_search Solr field for this document or nil if none
   def subject_other_subvy_search
     vals = @smods_rec.term_values([:subject, :temporal]) || []
     gvals = @smods_rec.term_values([:subject, :genre])
@@ -33,7 +58,7 @@ class SolrDocBuilder
     @smods_rec.subject.temporal.each { |n| 
       @logger.info("#{@druid} has subject temporal element with untranslated encoding: #{n.to_xml}") if !n.encoding.empty?
     }
-
+    
     return nil if vals.empty?
     vals
   end
