835b0670fb6df8561a46f17211ca7cb4f41c30e5
Naomi Dushay <ndushay@stanford.edu>
2014-02-12 12:09:58 -0800

    change notification email to Laura; change query for coll rec in verification; redo indenting in indexer.rb

--- indexer.rb
+++ indexer.rb
@@ -13,19 +13,18 @@ require 'harvestdor-indexer'
 # Base class to harvest from DOR via harvestdor gem
 class Indexer < Harvestdor::Indexer
 
-
   def initialize yml_path, solr_config_path,options = {}
-    @success_count=0
-    @error_count=0
-    @total_time_to_solr=0
-    @total_time_to_parse=0
-    @retries=0
+    @success_count = 0
+    @error_count = 0
+    @total_time_to_solr = 0
+    @total_time_to_parse = 0
+    @retries = 0
     @yml_path = yml_path
-    @validation_messages=''
-    solr_config=YAML.load_file(solr_config_path)
+    @validation_messages = ''
+    solr_config = YAML.load_file(solr_config_path)
     Indexer.config.configure(YAML.load_file(yml_path)) if yml_path    
     Indexer.config.configure options 
-    Indexer.config[:solr]={:url => solr_config["solr"]["url"], :read_timeout => 3600, :open_timeout => 3600}
+    Indexer.config[:solr] = {:url => solr_config["solr"]["url"], :read_timeout => 3600, :open_timeout => 3600}
     yield(Indexer.config) if block_given?
   end
 
@@ -47,86 +46,87 @@ class Indexer < Harvestdor::Indexer
   #  harvest the druids via OAI
   #   create a Solr document for each druid suitable for SearchWorks
   #   write the result to the SearchWorks Solr index
-    def harvest_and_index nocommit=false
-      start_time=Time.now
-      
-      logger.info("Started harvest_and_index at #{start_time}")
-      if whitelist.empty?
-        druids.threach(3) { |druid| index druid }
-      else
-        whitelist.threach(3) { |druid| index druid }
-      end
-      index_collection_druid
-      total_time=elapsed_time(start_time)
-      total_objects=@success_count+@error_count
-      logger.info("Finished harvest_and_index at #{Time.now}: final Solr commit returned")
-      logger.info("Total elapsed time for harvest and index: #{(total_time/60.0)} minutes")
-      logger.info("Beginning Commit.")
-      ## Commit our indexing job unless the :nocommit flag was passed
-      unless nocommit
-        solr_client.commit
-      else
-        puts "Skipping commit because :nocommit flag was passed"
-      end
-      verify
-      logger.info("Avg solr commit time per object (successful): #{@total_time_to_solr/@success_count} seconds") unless (@total_time_to_solr == 0 || @success_count == 0)
-      logger.info("Avg solr commit time per object (all): #{@total_time_to_solr/total_objects} seconds") unless (@total_time_to_solr == 0 || @error_count == 0 || total_objects == 0)
-      logger.info("Avg parse time per object (successful): #{@total_time_to_parse/@success_count} seconds") unless (@total_time_to_parse == 0 || @success_count == 0)
-      logger.info("Avg parse time per object (all): #{@total_time_to_parse/total_objects} seconds") unless (@total_time_to_parse == 0 || @error_count == 0 || total_objects == 0)
-      logger.info("Avg complete index time per object (successful): #{total_time/@success_count} seconds") unless (@success_count == 0)
-      logger.info("Avg complete index time per object (all): #{total_time/total_objects} seconds") unless (@error_count == 0 || total_objects == 0)
-      logger.info("Successful count: #{@success_count}")
-      if @found_in_solr_count == @success_count
-        logger.info("Records verified in solr: #{@found_in_solr_count}")
-      else
-        logger.info("Success Count and Solr count dont match, this might be a problem! Records verified in solr: #{@found_in_solr_count}")
-      end
-      logger.info("Error count: #{@error_count}")
-      logger.info("Retry count: #{@retries}")
-      logger.info("Total records processed: #{total_objects}")
-      send_notifications
-    end
+  def harvest_and_index nocommit=false
+    start_time=Time.now
     
-    def send_notifications
-      total_objects=@success_count+@error_count
-      notifications= Indexer::config.notification ? Indexer::config.notification : 'jdeering@stanford.edu'
-      subject="#{Indexer.config.log_name} is ready"
-      body=("Successful count: #{@success_count}\n")
-      if @found_in_solr_count == @success_count
-        body += ("Records verified in solr: #{@found_in_solr_count}\n")
-      else
-        body += ("Success Count and Solr count dont match, this might be a problem!\nRecords verified in solr: #{@found_in_solr_count}\n")
-      end
-      body +=("Error count: #{@error_count}\n")
-      body +=("Retry count: #{@retries}\n")
-      body +=("Total records processed: #{total_objects}\n")
-      body +=(@validation_messages)
-      opts={}
-      opts[:from_alias] = 'gryphondor'
-      opts[:server] = 'localhost'
-      opts[:from] = 'gryphondor@stanford.edu'
-      opts[:subject]=subject
-      opts[:body] = body
-      begin
-      send_email(notifications,opts)
-      rescue
-        logger.error('Failed to send email notification!')
-      end
+    logger.info("Started harvest_and_index at #{start_time}")
+    if whitelist.empty?
+      druids.threach(3) { |druid| index druid }
+    else
+      whitelist.threach(3) { |druid| index druid }
     end
-
+    index_collection_druid
+    total_time = elapsed_time(start_time)
+    total_objects = @success_count + @error_count
+    logger.info("Finished harvest_and_index at #{Time.now}: final Solr commit returned")
+    logger.info("Total elapsed time for harvest and index: #{(total_time/60.0)} minutes")
+    logger.info("Beginning Commit.")
+    ## Commit our indexing job unless the :nocommit flag was passed
+    unless nocommit
+      solr_client.commit
+    else
+      puts "Skipping commit because :nocommit flag was passed"
+    end
+    verify
+    logger.info("Avg solr commit time per object (successful): #{@total_time_to_solr/@success_count} seconds") unless (@total_time_to_solr == 0 || @success_count == 0)
+    logger.info("Avg solr commit time per object (all): #{@total_time_to_solr/total_objects} seconds") unless (@total_time_to_solr == 0 || @error_count == 0 || total_objects == 0)
+    logger.info("Avg parse time per object (successful): #{@total_time_to_parse/@success_count} seconds") unless (@total_time_to_parse == 0 || @success_count == 0)
+    logger.info("Avg parse time per object (all): #{@total_time_to_parse/total_objects} seconds") unless (@total_time_to_parse == 0 || @error_count == 0 || total_objects == 0)
+    logger.info("Avg complete index time per object (successful): #{total_time/@success_count} seconds") unless (@success_count == 0)
+    logger.info("Avg complete index time per object (all): #{total_time/total_objects} seconds") unless (@error_count == 0 || total_objects == 0)
+    logger.info("Successful count: #{@success_count}")
+    if @found_in_solr_count == @success_count
+      logger.info("Records verified in solr: #{@found_in_solr_count}")
+    else
+      logger.info("Success Count and Solr count dont match, this might be a problem! Records verified in solr: #{@found_in_solr_count}")
+    end
+    logger.info("Error count: #{@error_count}")
+    logger.info("Retry count: #{@retries}")
+    logger.info("Total records processed: #{total_objects}")
+    send_notifications
+  end
+  
+  def send_notifications
+    total_objects = @success_count + @error_count
+    notifications = Indexer::config.notification ? Indexer::config.notification : 'lauraw15@stanford.edu'
+    subject = "#{Indexer.config.log_name} is ready"
+    body = "Successful count: #{@success_count}\n")
+    if @found_in_solr_count == @success_count
+      body += "Records verified in solr: #{@found_in_solr_count}\n"
+    else
+      body += "Success Count and Solr count dont match, this might be a problem!\nRecords verified in solr: #{@found_in_solr_count}\n"
+    end
+    body += "Error count: #{@error_count}\n"
+    body += "Retry count: #{@retries}\n"
+    body += "Total records processed: #{total_objects}\n"
+    body += @validation_messages
+    opts = {}
+    opts[:from_alias] = 'gryphondor'
+    opts[:server] = 'localhost'
+    opts[:from] = 'gryphondor@stanford.edu'
+    opts[:subject] = subject
+    opts[:body] = body
+    begin
+    send_email(notifications,opts)
+    rescue
+      logger.error('Failed to send email notification!')
+    end
+  end
 
   # @return [boolean] true if the collection has a catkey
   def collection_is_mergable?
-    sdb=SolrDocBuilder.new(collection_druid, harvestdor_client, logger)
+    sdb = SolrDocBuilder.new(collection_druid, harvestdor_client, logger)
     if sdb.catkey
       logger.info "Collection #{collection_druid} is being merged with cat key #{sdb.catkey}"
     end
     false
   end
+  
   # @return [String]The collection object catkey or nil if none exists
   def catkey
     @catkey ||= SolrDocBuilder.new(collection_druid, harvestdor_client, logger).catkey
   end
+  
   # Create a solr document for the collection druid suitable for searchworks
   # write the result to the SearchWorks Solr Index
   # @param [String] druid 
@@ -135,21 +135,21 @@ class Indexer < Harvestdor::Indexer
       begin
         logger.debug "Merging collection object #{collection_druid} into #{catkey}"
         RecordMerger.merge(collection_druid,catkey)
-        @success_count+=1
+        @success_count += 1
       rescue => e
         logger.error "Failed to merge collection object #{collection_druid}: #{e.message}"
-         @error_count+=1
+         @error_count += 1
       end
     else
-    begin
-      logger.debug "Indexing collection object #{collection_druid}"
-      solr_client.add(sw_solr_doc(collection_druid)) unless collection_druid.nil?
-      @success_count+=1
-      # update DOR object's workflow datastream??   for harvest?  for indexing?
-    rescue => e
-      logger.error "Failed to index collection object #{collection_druid}: #{e.message}"
-       @error_count+=1
-    end
+      begin
+        logger.debug "Indexing collection object #{collection_druid}"
+        solr_client.add(sw_solr_doc(collection_druid)) unless collection_druid.nil?
+        @success_count += 1
+        # update DOR object's workflow datastream??   for harvest?  for indexing?
+      rescue => e
+        logger.error "Failed to index collection object #{collection_druid}: #{e.message}"
+        @error_count += 1
+      end
     end
   end
 
@@ -178,6 +178,7 @@ class Indexer < Harvestdor::Indexer
   def self.language_hash
     @@language_hash ||= {}
   end
+  
   # create Solr doc for the druid and add it to Solr, unless it is on the blacklist.  
   #  NOTE: don't forget to send commit to Solr, either once at end (already in harvest_and_index), or for each add, or ...
   def index druid
@@ -185,11 +186,11 @@ class Indexer < Harvestdor::Indexer
       logger.info("Druid #{druid} is on the blacklist and will have no Solr doc created")
     else
       begin
-      logger.info "indexing #{druid}"
+        logger.info "indexing #{druid}"
         solr_add(sw_solr_doc(druid),druid)
-        @success_count+=1
+        @success_count += 1
       rescue => e
-        @error_count+=1
+        @error_count += 1
         logger.error "Failed to index #{druid}: #{e.message} #{e.backtrace}"
       end
     end
@@ -204,7 +205,7 @@ class Indexer < Harvestdor::Indexer
     sdb = SolrDocBuilder.new(druid, harvestdor_client, logger)
     doc_hash = sdb.doc_hash
     sdb.validate.each do |msg|
-      @validation_messages+=msg+"\n"
+      @validation_messages += msg + "\n"
     end
     # determine collection druids and their titles and add to solr doc
     coll_druids = sdb.collection_druids    
@@ -216,19 +217,19 @@ class Indexer < Harvestdor::Indexer
           @coll_hash[coll_druid] = identity_md_obj_label(coll_druid)
         end
         if !Indexer.format_hash.keys.include? coll_druid
-          Indexer.format_hash[coll_druid]={}
+          Indexer.format_hash[coll_druid] = {}
         end
         if !Indexer.language_hash.keys.include? coll_druid
-          Indexer.language_hash[coll_druid]={}
+          Indexer.language_hash[coll_druid] = {}
         end
         #store the format(s) of this object with each collection, so when the collections are being indexed, they get all of the formats of the members
         if doc_hash[:format]
           if doc_hash[:format].kind_of?(Array)
             doc_hash[:format].each do |format|
-              Indexer.format_hash[coll_druid][format]=format
+              Indexer.format_hash[coll_druid][format] = format
             end
           else
-            Indexer.format_hash[coll_druid][doc_hash[:format]]=doc_hash[:format]
+            Indexer.format_hash[coll_druid][doc_hash[:format]] = doc_hash[:format]
           end
         end
         if doc_hash[:language]
@@ -237,69 +238,72 @@ class Indexer < Harvestdor::Indexer
               Indexer.language_hash[coll_druid][lang]=lang
             end
           else
-            Indexer.language_hash[coll_druid][doc_hash[:language]]=doc_hash[:language]
+            Indexer.language_hash[coll_druid][doc_hash[:language]] = doc_hash[:language]
           end
         end
-          if catkey
-            doc_hash[:collection] << catkey
-          else
-        doc_hash[:collection] << coll_druid
-        end
-        doc_hash[:collection_with_title] << "#{coll_druid}-|-#{coll_hash[coll_druid]}"
-        }
+        if catkey
+          doc_hash[:collection] << catkey
+        else
+      doc_hash[:collection] << coll_druid
       end
-      doc_hash[:url_fulltext] = "#{Indexer.config.purl}/#{druid}"
-      doc_hash
+      doc_hash[:collection_with_title] << "#{coll_druid}-|-#{coll_hash[coll_druid]}"
+      }
     end
-    
-    #count the number of records in solr for this collection (and the collection record itself), to compare against the number the indexer thinks it indexed.
-    def verify
-      params={:fl => 'id', :rows => 1000}
-      col= catkey ? catkey : collection_druid
-      params[:fq] = "collection:\"#{col}\""
-      params[:start] ||= 0
-      resp = solr_client.get 'select', :params => params
-      @found_in_solr_count = resp['response']['numFound'].to_i
-
-      #get the collection record too
-      params.delete(:fq)
-      params[:fl] = 'id, url_fulltext'
-      params[:fq] = "id:\"#{col}\""
-      resp = solr_client.get 'select', :params => params
-      puts resp.inspect
-      resp['response']['docs'].each do |doc|
-        if doc['url_fulltext'] and doc['url_fulltext'].to_s.include?('http://purl.stanford.edu/'+doc['id'])
-          @found_in_solr_count += 1
-        end
-      end
-      @found_in_solr_count
-    end
-    
-
-    def solr_client
-      @solr_client ||= RSolr.connect(Indexer.config.solr.to_hash)
-    end
-
-    # given a druid, get its objectLabel from its purl page identityMetadata
-    # @param [String] druid, e.g. ab123cd4567
-    # @return [String] the value of the <objectLabel> element in the identityMetadata for the object
-    def identity_md_obj_label druid
-      ng_imd = harvestdor_client.identity_metadata druid
-      # TODO: create nom-xml terminology for identityMetadata in harvestdor?
-      ng_imd.xpath('identityMetadata/objectLabel').text
-    end
-    def send_email(to,opts={})
-      opts[:server]      ||= 'localhost'
-      opts[:from]        ||= 'email@example.com'
-      opts[:from_alias]  ||= 'Example Emailer'
-      opts[:subject]     ||= "You need to see this"
-      opts[:body]        ||= "Important stuff!"
-      mail = Mail.new do
-        from    opts[:from]
-        to      to
-        subject opts[:subject]
-        body    opts[:body]
-      end
-      mail.deliver!
-    end
+    doc_hash[:url_fulltext] = "#{Indexer.config.purl}/#{druid}"
+    doc_hash
   end
+  
+  #count the number of records in solr for this collection (and the collection record itself), to compare against the number the indexer thinks it indexed.
+  def verify
+    params = {:fl => 'id', :rows => 1000}
+    coll_rec_id = catkey ? catkey : collection_druid
+    params[:fq] = "collection:\"#{coll_rec_id}\""
+    params[:start] ||= 0
+    resp = solr_client.get 'select', :params => params
+    @found_in_solr_count = resp['response']['numFound'].to_i
+
+    # get the collection record too
+    params.delete(:fq)
+    params[:fl] = 'id, url_fulltext'
+    params[:qt] = 'document'
+    params[:id] = coll_rec_id
+    resp = solr_client.get 'select', :params => params
+    puts resp.inspect
+    resp['response']['docs'].each do |doc|
+      if doc['url_fulltext'] and doc['url_fulltext'].to_s.include?('http://purl.stanford.edu/' + doc['id'])
+        @found_in_solr_count += 1
+      end
+    end
+    @found_in_solr_count
+  end
+  
+
+  def solr_client
+    @solr_client ||= RSolr.connect(Indexer.config.solr.to_hash)
+  end
+
+  # given a druid, get its objectLabel from its purl page identityMetadata
+  # @param [String] druid, e.g. ab123cd4567
+  # @return [String] the value of the <objectLabel> element in the identityMetadata for the object
+  def identity_md_obj_label druid
+    ng_imd = harvestdor_client.identity_metadata druid
+    # TODO: create nom-xml terminology for identityMetadata in harvestdor?
+    ng_imd.xpath('identityMetadata/objectLabel').text
+  end
+  
+  def send_email(to, opts = {})
+    opts[:server]      ||= 'localhost'
+    opts[:from]        ||= 'email@example.com'
+    opts[:from_alias]  ||= 'Example Emailer'
+    opts[:subject]     ||= "You need to see this"
+    opts[:body]        ||= "Important stuff!"
+    mail = Mail.new do
+      from    opts[:from]
+      to      to
+      subject opts[:subject]
+      body    opts[:body]
+    end
+    mail.deliver!
+  end
+  
+end
