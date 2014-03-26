d042fd912fd2888ce13fba6add8cdefd88b22e97
Naomi Dushay <ndushay@stanford.edu>
2014-03-25 19:27:50 -0700

    remove unnec require in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,6 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 
 require 'indexer'
-require 'solr_doc_builder'
 require 'equivalent-xml'
 
 #RSpec.configure do |config|
