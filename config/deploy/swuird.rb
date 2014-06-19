e317d466492b8c969058f7f08ba19ed0d1f6e4c9
Naomi Dushay <ndushay@stanford.edu>
2014-06-19 15:32:27 -0700

    swuird deploy script no longer needs to override scripts

--- config/deploy/swuird.rb
+++ config/deploy/swuird.rb
@@ -6,5 +6,3 @@ Capistrano::OneTimeKey.generate_one_time_key!
 set :deploy_to, "/home/#{fetch(:user)}/gdor-indexer-swuird"
 # temporary:  use item-level-merge branch on harvestdor-dev!
 set :branch, "swuird"
-# temporary:  scripts for groups of collections
-set :linked_files, %w{.ruby-version config/solr.yml bin/index_prod_collections.sh bin/index_stage_collections.sh bin/index-prod-image.sh bin/index-prod-hydrus.sh}
