f24f2071d1072c9e7aac5c42287b572f863f576e
Naomi Dushay <ndushay@stanford.edu>
2014-08-24 13:21:39 -0700

    adjust cap deploy to not use defunct scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :user, "lyberadmin"
 set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 
 set :linked_dirs, %w(logs config/collections tmp solrmarc-sw)
-set :linked_files, %w{.ruby-version config/solr.yml bin/index_prod_collections.sh bin/index_stage_collections.sh bin/index-prod-image.sh bin/index-prod-hydrus.sh}
+set :linked_files, %w{.ruby-version config/solr.yml bin/index-prod-image.sh bin/index-prod-hydrus.sh}
 
 set :stages, %W(dev stage prod swuird)
 
