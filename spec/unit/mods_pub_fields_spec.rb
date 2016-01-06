408c001a84b28206ede9626fc3fdfe22cf27f29a
Naomi Dushay <ndushay@stanford.edu>
2016-01-06 12:19:19 -0800

    mods_pub_fields_spec: add tests for imprint_display

--- spec/unit/mods_pub_fields_spec.rb
+++ spec/unit/mods_pub_fields_spec.rb
@@ -19,8 +19,19 @@ describe GDor::Indexer::ModsFields do
     GDor::Indexer::SolrDocBuilder.new(resource, lgr)
   end
 
+  let(:sdb) { sdb_for_mods(mods_xml) }
+
   context 'publication date fields' do
-    let(:sdb) { sdb_for_mods(mods_xml) }
+
+    RSpec.shared_examples "expected" do |solr_field_sym, mods_field_val, exp_val|
+      it "#{exp_val} for #{mods_field_val}" do
+        m = mods_origin_info_start_str +
+              "<dateIssued>#{mods_field_val}</dateIssued>" +
+            mods_origin_info_end_str
+        sdb = sdb_for_mods(m)
+        expect(sdb.doc_hash_from_mods[solr_field_sym]).to eq exp_val
+      end
+    end
 
     # TODO:  this test is in the process of being incorporated into individual field tests below
     it 'populates all date fields' do
@@ -35,7 +46,7 @@ describe GDor::Indexer::ModsFields do
       expect(doc_hash[:pub_year_w_approx_isi]).to eq('19th century') # covered in tests below
       expect(doc_hash[:publication_year_isi]).to eq('1800')
       expect(doc_hash[:pub_year_tisim]).to eq('1800') # covered in pub_year_tisim tests
-      expect(doc_hash[:imprint_display]).to eq('blah blah 19th century blah blah')
+      expect(doc_hash[:imprint_display]).to eq('blah blah 19th century blah blah') # covered in imprint_display tests
     end
 
     context 'pub_date_sort' do
@@ -296,7 +307,7 @@ describe GDor::Indexer::ModsFields do
         expect(doc_hash[:pub_date_sort]).to eq('1800') # covered in pub_date_sort tests
         expect(doc_hash[:pub_year_tisim]).to eq('1800') # covered in pub_year_tisim tests
         expect(doc_hash[:publication_year_isi]).to eq('1800')
-        expect(doc_hash[:imprint_display]).to eq('19th century')
+        expect(doc_hash[:imprint_display]).to eq('19th century') # covered in imprint_display tests
       end
 
       it 'works on explicit 3 digit dates' do
@@ -309,7 +320,7 @@ describe GDor::Indexer::ModsFields do
         expect(doc_hash[:pub_date]).to eq('966')  # deprecated due to new pub_year_ fields
         expect(doc_hash[:pub_year_tisim]).to eq('0966') # covered in pub_year_tisim tests
         expect(doc_hash[:publication_year_isi]).to eq('0966')
-        expect(doc_hash[:imprint_display]).to eq('966')
+        expect(doc_hash[:imprint_display]).to eq('966') # covered in imprint_display tests
       end
       it 'works on 3 digit century dates' do
         m = "<mods #{ns_decl}><originInfo>
@@ -321,7 +332,7 @@ describe GDor::Indexer::ModsFields do
         expect(doc_hash[:pub_year_tisim]).to eq('0800') # covered in pub_year_tisim tests
         expect(doc_hash[:pub_date]).to eq('9th century') # deprecated due to new pub_year_ fields
         expect(doc_hash[:publication_year_isi]).to eq('0800')
-        expect(doc_hash[:imprint_display]).to eq('9th century')
+        expect(doc_hash[:imprint_display]).to eq('9th century') # covered in imprint_display tests
       end
       it 'works on 3 digit BC dates' do
         m = "<mods #{ns_decl}><originInfo>
@@ -332,9 +343,28 @@ describe GDor::Indexer::ModsFields do
         expect(doc_hash[:pub_date_sort]).to eq('-700') # covered in pub_date_sort tests
         expect(doc_hash[:pub_year_tisim]).to be_nil # covered in pub_year_tisim tests
         expect(doc_hash[:pub_date]).to eq('300 B.C.') # deprecated due to new pub_year_ fields
-        expect(doc_hash[:imprint_display]).to eq('300 B.C.')
+        expect(doc_hash[:imprint_display]).to eq('300 B.C.') # covered in imprint_display tests
         # doc_hash[:creation_year_isi].should =='-300'
       end
     end # difficult pub dates
   end # publication date fields
+
+  context 'imprint_display' do
+    # FIXME:  it should be using a method returning a better string than just year
+    it 'imprint_display calls deprecated Stanford::Mods::Record instance pub_date_display' do
+      expect(sdb.smods_rec).to receive(:pub_date_display)
+      sdb.doc_hash_from_mods[:imprint_display]
+    end
+    it_behaves_like "expected", :imprint_display, '1945', '1945'
+    it_behaves_like "expected", :imprint_display, '945', '945'
+    it_behaves_like "expected", :imprint_display, '16--', '16--'
+    it_behaves_like "expected", :imprint_display, '9--', '9--'
+    it_behaves_like "expected", :imprint_display, '19th century', '19th century'
+    it_behaves_like "expected", :imprint_display, '9th century', '9th century'
+    it_behaves_like "expected", :imprint_display, 'blah June 4, 1594; blah 1596', 'blah June 4, 1594; blah 1596'
+    it_behaves_like "expected", :imprint_display, 'Aug. 3rd, 1886', 'Aug. 3rd, 1886'
+    it_behaves_like "expected", :imprint_display, 'Aug. 3rd, [18]86?', 'Aug. 3rd, [18]86?'
+    it_behaves_like "expected", :imprint_display, 'early 1890s', 'early 1890s'
+    it_behaves_like "expected", :imprint_display, '1865-6', '1865-6'
+  end
 end
